45100f0d90866fc7795c2e2a1eb3f9479048b074
Naufal0067 <naufalopang1928@gmail.com>
2024-02-26 15:33:10 +0700

    Add project files.

---
+++ PertemuanPertamaKelasB/PertemuanPertamaKelasB.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+	// numeric nPanjang, nLebar, nLuas
+	// display 'Masukan Panjang= '
+	// accept nPanjang
+	// display 'Masukan Lebar = '
+	// accept nLebar
+	//compute nLuas = npanjang * nLebar
+	// display 'Luasnya adalah = ' + nLuas
+
+	int nPanjang, nLebar, nLuas;
+	
+
